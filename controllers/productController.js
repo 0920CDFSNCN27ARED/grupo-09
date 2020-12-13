1d23e5d4615a202fe6a6d608e9f5c0f0b96b6aee
MatiasCerdeira <maticerdeira@hotmail.com>
2020-12-13 19:31:20 -0300

    Formularios de creacion y edicion de productos

--- controllers/productController.js
+++ controllers/productController.js
@@ -21,6 +21,35 @@ const controller = {
       requiredProduct: requiredProduct,
     });
   },
+
+  showCreate: (req, res) => {
+    res.render('products/create');
+  },
+
+  create: (req, res) => {
+    const message = 'Product Created: ' + JSON.stringify(req.body);
+    res.send(message);
+  },
+
+  showEdit: (req, res) => {
+    const products = getProducts();
+    const requiredProduct = products.find((prod) => {
+      return prod.id == req.params.id;
+    });
+    if (requiredProduct == null) {
+      return res
+        .status(404)
+        .send('404 not found. <br> ¡Houston, poseemos problemas!');
+    }
+    res.render('products/edit', {
+      product: requiredProduct,
+    });
+  },
+
+  edit: (req, res) => {
+    const message = 'Product Edited: ' + JSON.stringify(req.body);
+    res.send(message);
+  },
 };
 
 module.exports = controller;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,9 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const productsController = require('../controllers/productController');
+const { route } = require('./mainRoutes');
 
 router.get('/', productsController.index);
 
+router.get('/create', productsController.showCreate);
+
 router.get('/:id', productsController.getOne);
+
+router.get('/:id/edit', productsController.showEdit);
+
+router.post('/', productsController.create);
+
+router.put('/id', productsController.edit);
 
 module.exports = router;
